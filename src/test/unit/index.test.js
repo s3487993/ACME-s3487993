819e9a30169120c0c87da9db6f008057d3b39c75
s3487993 <s3487993@student.rmit.edu.au>
2020-04-17 00:43:31 +1000

    Add linting to circleci

--- src/test/unit/index.test.js
+++ src/test/unit/index.test.js
@@ -5,11 +5,6 @@ var expect = require('expect.js');
 describe('models/index', function () {
   it('returns the task model', function () {
     var models = require('../../models');
-    expect(models.Tasks).to.be.ok();
-  });
-
-  it('returns the user model', function () {
-    var models = require('../../models');
-    expect(models.Users).to.be.ok();
+    expect(models.Task;
   });
 });
